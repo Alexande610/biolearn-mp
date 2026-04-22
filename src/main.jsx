820ec01350cc6cd56e9a3becd2270466d2fa6791
MintPhut <nguyenminhphuc06102019@gmail.com>
2026-04-22 19:51:10 +0700

    update biolearn-mp project

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 // Molstar CSS cho molecular viewer
 import 'molstar/lib/mol-plugin-ui/skin/light.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
